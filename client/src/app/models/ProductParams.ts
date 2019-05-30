568f604f2da26148ab21bf49b6feaeae5e8e9f11
Artem Harlamenko <4.harlamenko@gmail.com>
2019-05-31 01:10:46 +0300

    realized search

--- client/src/app/models/ProductParams.ts
+++ client/src/app/models/ProductParams.ts
@@ -1,11 +1,17 @@
 interface IProductParams {
     sex?: string;
     type?: string;
+    count: number;
+    lang?: string;
+    qs?: string;
 };
 
 export class ProductParams implements IProductParams {
     sex?: string;
     type?: string;
+    count: number;
+    lang?: string;
+    qs?: string;
     constructor() {
         this.type = null;
         this.sex = null;
