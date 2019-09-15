8ecec8428d42ec78422f89db4d2c2a2f3322b684
Artem Harlamenko <4.harlamenko@gmail.com>
2019-09-15 18:35:31 +0300

    added custom input

---
+++ client/src/app/shared/components/input/input.component.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit, Input, ViewChild, ElementRef, Renderer2, forwardRef } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+
+@Component({
+  selector: 'custom-input',
+  templateUrl: './input.component.html',
+  styleUrls: ['./input.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => InputComponent),
+      multi: true,
+    }
+  ]
+})
+export class InputComponent implements ControlValueAccessor {
+  public value: string = '';
+  @Input() type: string = 'text';
+  @Input() id: string = '';
+  @Input() placeholder: string = '';
+
+  @ViewChild('inputElement') private _inputElement: ElementRef;
+  get inputElement():ElementRef {
+    return this._inputElement;
+  }
+
+  constructor(
+    private _renderer: Renderer2
+  ) { }
+
+  private _onChange = (_: any) => {};
+  private _onTouched = () => {};
+
+  writeValue(obj: any): void {
+    this.value = obj;
+  }
+  registerOnChange(fn: any): void {
+    this._onChange = fn;
+  }
+  registerOnTouched(fn: any): void {
+    this._onTouched = fn;
+  }
+  setDisabledState?(isDisabled: boolean): void {
+    this._renderer.setProperty(this._inputElement.nativeElement, 'disabled', isDisabled);
+  }
+  onChange(event: any) {
+    this._onChange(event.target.value);
+  }
+  onKeyUp(event: any) {
+    this._onChange(event.target.value);
+  }
+  onBlur(event: any) {
+    this._onTouched();
+  }
+}
