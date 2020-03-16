6fb14554e479ed8afceb33a50a02f083fbb39511
Artem Harlamenko <4.harlamenko@gmail.com>
2020-03-16 22:11:19 +0300

    fix: about page

--- client/src/app/about/about.module.ts
+++ client/src/app/about/about.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
 import { AboutComponent } from './containers/about/about.component';
+import { AboutRoutingModule } from './about.routing-module';
 
 @NgModule({
   imports: [
+    AboutRoutingModule,
     SharedModule,
   ],
   declarations: [
